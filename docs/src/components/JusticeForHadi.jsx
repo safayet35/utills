b121d03445c05c68da900c08fa92643b391e9809
safayet35 <sf.safayet35@gmail.com>
2026-01-05 13:51:15 +0600

    change hadi name

--- docs/src/components/JusticeForHadi.jsx
+++ docs/src/components/JusticeForHadi.jsx
@@ -3,7 +3,7 @@ import hadi from "/assets/hadi.jpg";
 const JusticeForHadi = () => {
     return (
         <div className="w-full py-2 flex items-center justify-center bg-red-500 text-white gap-2">
-            <p className="font-bold">We want justice for hadi</p>
+            <p className="font-bold">We want justice for Osman hadi</p>
             <HandFist />
             <img className="w-14 rounded-full" src={hadi} alt="" />
         </div>

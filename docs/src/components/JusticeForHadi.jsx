3f339f4ffeb57228da076cc8b2af1ffda2aad153
safayet35 <sf.safayet35@gmail.com>
2026-01-05 13:43:17 +0600

    add justice for hadi banner

---
+++ docs/src/components/JusticeForHadi.jsx
@@ -0,0 +1,13 @@
+import { HandFist } from "lucide-react";
+import hadi from "/assets/hadi.jpg";
+const JusticeForHadi = () => {
+    return (
+        <div className="w-full py-2 flex items-center justify-center bg-red-500 text-white gap-2">
+            <p className="font-bold">We want justice for hadi</p>
+            <HandFist />
+            <img className="w-14 rounded-full" src={hadi} alt="" />
+        </div>
+    );
+};
+
+export default JusticeForHadi;
--- docs/src/layout/AppLayout.jsx
+++ docs/src/layout/AppLayout.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
 import Header from "../components/header/Header";
+import JusticeForHadi from "../components/JusticeForHadi.jsx";
 import { useState } from "react";
 const AppLayout = () => {
     return (
         <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
             {/* Sidebar (fixed) */}
-            <Sidebar
-                
-            />
+            <Sidebar />
 
             {/* Main content */}
             <div className="lg:pl-72">
                 <Header />
+                <JusticeForHadi/>
                 <main className="p-4 md:px-8 md:py-2 prose max-w-none">
                     <Outlet />
                 </main>
